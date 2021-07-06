50473883800f50f42a06bb38b87e18e4c0ddd8b2
Leonardo Gardini <leogardini10.1@gmail.com>
2021-07-06 14:27:47 -0300

    Created the base page of information about the wallet creation

--- src/modules/wallet/components/ProcedureInfo.tsx
+++ src/modules/wallet/components/ProcedureInfo.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 
 function ProcedureInfo() {
   return (
-    <div>
-      <p>Warning: This is a precudere info page!</p>
-    </div>
+    <Container maxWidth="sm">
+      <Box style={{ display: "flex", width: "100%" }}>
+        <Typography align="center">
+          <b>To activate 2fa you must save secret phrase (12 words).</b>
+        </Typography>
+      </Box>
+
+      <Box style={{ display: "flex", width: "100%" }}>
+        <Typography align="center">
+          Please save your secret phrase (12 words)!
+        </Typography>
+      </Box>
+    </Container>
   );
 }
 
