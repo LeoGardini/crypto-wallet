168cad7007d37c184d2ee8dbf1cc75a7c72a34eb
Leonardo Gardini <leogardini10.1@gmail.com>
2021-07-11 14:35:45 -0300

    Project dependencies reorganization

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,13 @@
 import { composeWithDevTools } from "redux-devtools-extension";
-import { combineReducers, createStore, compose, AnyAction, Store } from "redux";
+import { combineReducers, createStore, compose } from "redux";
 
 import mnemonics from "./reducers";
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-let store: Store<any, AnyAction>;
-if (!IS_PRODUCTION) {
-  const applied = compose(composeWithDevTools());
-  const combined = combineReducers({ mnemonics });
-  store = createStore(combined, {}, applied);
-} else {
-  const combined = combineReducers({ mnemonics });
-  store = createStore(combined, {});
-}
+const combined = combineReducers({ mnemonics });
+const store = IS_PRODUCTION
+  ? createStore(combined)
+  : createStore(combined, {}, compose(composeWithDevTools()));
 
 export default store;
